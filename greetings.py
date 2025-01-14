99d2f4fdb9b3b85538c08c1808cdddc13414e024
Dainas42 <zaitzeva.anastasia27@yandex.ru>
2025-01-14 21:58:05 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,13 @@
+
+username = "Имя пользователя"
+print(username)
+title = "Заголовок заметки"
+print(title)
+content = "Описание заметки"
+print(content)
+status = "Статус заметки"
+print(status)
+created_date = "Дата создания заметки"
+print(created_date)
+issue_date = "Дата истечния заметки(дедлайн)"
+print(issue_date)
