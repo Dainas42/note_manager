bcfa1567cd71d937e17eef077813a18ec32f5112
Dainas42 <zaitzeva.anastasia27@yandex.ru>
2025-01-15 23:03:58 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,12 @@
+
+print("Введите заголовок заметки: ")
+while True:
+    title = input()
+    if title.strip() == "":
+        break
+
+print("Заполните содержание заметки: ")
+while True:
+    title = input()
+    if title.strip() == "":
+        break
