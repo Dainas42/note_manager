c3ac5f2989ba718857d42927b188a3e92e345e73
Dainas42 <zaitzeva.anastasia27@yandex.ru>
2025-01-21 21:27:36 +0300

    Update date_changer.py

--- date_changer.py
+++ date_changer.py
@@ -1,4 +1,4 @@
-created_date = input("Введите дату начала: ")
+created_date = input("Введите дату начала - день.месяц.год: ")
 print(created_date[0:5])
-issue_date = input("Введите дату истечения: ")
+issue_date = input("Введите дату истечения - день.месяц.год: ")
 print(issue_date[0:5])
