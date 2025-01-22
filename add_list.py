a0a4135f7331d7245d78114539e452209c2ff826
Dainas42 <zaitzeva.anastasia27@yandex.ru>
2025-01-22 14:47:47 +0300

    Update add_list.py

--- add_list.py
+++ add_list.py
@@ -1,6 +1,11 @@
+note = {}
+note["titles"] = []
 
+print("Заголовок заметки: ")
 title1 = input("")
 title2 = input("")
 title3 = input("")
 titles = (title1, title2, title3)
+
+for title in note["titles"]:print(title)
 print(titles)
